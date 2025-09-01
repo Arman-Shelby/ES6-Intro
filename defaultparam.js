33683607b2dfae27c9aa7c889d5a7a60a8357fd3
Arman Mir <mirarman8583@gmail.com>
2025-09-01 07:19:41 +0600

    default parameter for not provided values

--- defaultparam.js
+++ defaultparam.js
@@ -1,3 +1,32 @@
-function sum(num1 , num2){
-    
+function sum(num1 = 0 , num2 = 5){ //default value kaj korbe jokhon function e kono argument pass kora hobe na
+    const total = num1 + num2;
+
+    console.log(total);
 }
+sum(10); // NaN -->    Not a Number
+sum(10, 20); // 30
+sum(); // 5
+
+console.log('------------------');
+
+function multiply(a = 0 , b = 0){ // multiply korar somoy man 0 to deya jabe na 
+    const result = a * b;
+   console.log(result);
+}
+
+multiply(10); 
+
+console.log('----------------')
+
+function fullName (first , last=''){
+    const name = first + last 
+    console.log(name)
+}
+fullName('Satul')
+
+
+// add , sub korle maaan ==== 0
+// multiply ----1
+// string ===== '
+// array = [] --empty
+//object = {} --empty
